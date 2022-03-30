2fb2addfe76cb416b9f2f1736f6b2429bc093502
Tan-CM <tanchorming@gmail.com>
2022-03-30 09:07:50 +0800

    Updated assignment

--- infiniteQuery.go
+++ infiniteQuery.go
@@ -1,5 +1,31 @@
 package main
 
+import "fmt"
+
 func main() {
 	//Insert your code here
+	// infinite for loop like while (true) {}
+	var number int
+	var dummy string
+	for {
+		fmt.Println("Enter a number, -1 to Exit")
+		fmt.Scanf("%d", &number)
+		fmt.Scanf("%s", &dummy)
+
+		// Exit for any number less than 0
+		if number < 0 {
+			break
+		}
+
+		if number%2 == 1 {
+			fmt.Println("Number is Odd")
+		} else {
+			fmt.Println("Number is Even")
+		}
+		if number < 10 {
+			fmt.Println("Single Digit")
+		} else {
+			fmt.Println("More than One Digit")
+		}
+	}
 }
